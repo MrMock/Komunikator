107e853503383c05a277fcb48ef2b183e205be46
Grzegorz Lalko <grzegorzlalko@gmail.com>
2024-01-25 00:19:47 +0100

    wyswietlanie kanalow oraz wiadomosci

--- js/welcome.js
+++ js/welcome.js
@@ -1,17 +1,3 @@
-var accessToken; // Zmienna globalna
-
-function getCookie(name) {
-    var value = "; " + document.cookie;
-    var parts = value.split("; " + name + "=");
-    if (parts.length == 2) return parts.pop().split(";").shift();
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-    accessToken = getCookie('PHPSESSID'); // Przypisanie wartości do zmiennej globalnej
-    setupWebSocket(accessToken);
-    fetchPublicChannels();
-});
-
 function setupWebSocket(accessToken) {
     var hostUrl = "wss://webapp-oiihp.ondigitalocean.app/socket.io/";
     var socket = new WebSocket(hostUrl + '?access-token=' + accessToken + "&EIO=4&transport=websocket");
@@ -36,8 +22,8 @@ function setupWebSocket(accessToken) {
 function fetchPublicChannels() {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "https://webapp-oiihp.ondigitalocean.app/api/channels/public", true);
-    xhr.setRequestHeader("Authorization", "Bearer " + accessToken);
-
+    xhr.setRequestHeader("x-access-token", accessToken);
+	
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4) {
             if (xhr.status == 200) {
@@ -58,7 +44,245 @@ function fetchPublicChannels() {
 function displayChannels(channelsData, containerId) {
     var container = document.getElementById(containerId);
     container.innerHTML = '';
+
     channelsData.channels.forEach(function(channel) {
-        container.innerHTML += '<p>' + channel.name + ' (ID: ' + channel.id + ')</p>';
+        // Utworzenie przycisku dla każdego kanału
+        var channelButton = document.createElement('button');
+        channelButton.textContent = channel.name;
+        channelButton.classList.add('channel-button');
+        channelButton.setAttribute('data-channel-id', channel.id);
+
+        // Dodanie obsługi zdarzenia kliknięcia
+        channelButton.addEventListener('click', function() {
+            selectChannel(channel.id);
+        });
+
+        container.appendChild(channelButton);
     });
+}
+
+function displayChannels(channelsData, containerId) {
+    var container = document.getElementById(containerId);
+    container.innerHTML = '';
+    var isPublicChannel = containerId === 'publicChannels';
+
+    channelsData.channels.forEach(function(channel) {
+        var channelButton = document.createElement('button');
+        channelButton.textContent = channel.name;
+        channelButton.classList.add('channel-button', isPublicChannel ? 'public-channel' : 'user-channel');
+        channelButton.setAttribute('data-channel-id', channel.id);
+
+        channelButton.addEventListener('click', function() {
+            if (isPublicChannel) {
+                selectChannel(channel.id, true);
+            } else {
+                selectUserChannel(channel.id);
+            }
+        });
+
+        container.appendChild(channelButton);
+    });
+}
+document.addEventListener("DOMContentLoaded", function() {
+    var accessToken = localStorage.getItem('accessToken'); // lub inny sposób pobrania tokenu
+    setupWebSocket(accessToken);
+	fetchUserChannels();
+    fetchPublicChannels();  // Wywołanie funkcji fetchPublicChannels po załadowaniu strony
+});
+
+function selectPublicChannel(channelId) {
+    // Usuń zaznaczenie z poprzednio wybranego publicznego kanału
+    var previouslySelectedPublic = document.querySelector('.public-channel.selected');
+    if (previouslySelectedPublic) {
+        previouslySelectedPublic.classList.remove('selected');
+    }
+
+    // Zaznacz aktualnie wybrany publiczny kanał
+    var selectedPublicChannel = document.querySelector('.public-channel[data-channel-id="' + channelId + '"]');
+    if (selectedPublicChannel) {
+        selectedPublicChannel.classList.add('selected');
+    }
+	    document.getElementById('messagesDisplay').innerHTML = ''; // Czyszczenie wiadomości
+    fetchChannelMessages(channelId);
+	fetchChannelMessages(channelId);
+}
+
+function selectUserChannel(channelId) {
+    // Usuń zaznaczenie z poprzednio wybranego kanału użytkownika
+    var previouslySelected = document.querySelector('.user-channel.selected');
+    if (previouslySelected) {
+        previouslySelected.classList.remove('selected');
+    }
+
+    // Zaznacz aktualnie wybrany kanał użytkownika
+    var selectedChannel = document.querySelector('.user-channel[data-channel-id="' + channelId + '"]');
+    if (selectedChannel) {
+        selectedChannel.classList.add('selected');
+    }
+
+
+	var selectedChannel = document.querySelector('.user-channel[data-channel-id="' + channelId + '"]');
+    if (selectedChannel) {
+        selectedChannel.classList.add('selected');
+    }
+	    document.getElementById('messagesDisplay').innerHTML = ''; // Czyszczenie wiadomości
+    fetchChannelMessages(channelId);
+	 fetchChannelMessages(channelId);
+    // Wyświetl przycisk "Opuść kanał" i ukryj przycisk "Kopiuj ID kanału"
+    document.getElementById('leaveChannelButton').style.display = 'block';
+    document.getElementById('copyChannelIdButton').style.display = 'none';
+    currentChannelId = channelId; // Zapisz ID wybranego kanału użytkownika
+}
+
+function fetchUserChannels() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://webapp-oiihp.ondigitalocean.app/api/channels/user", true);
+    xhr.setRequestHeader("x-access-token", accessToken);
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                try {
+                    var channels = JSON.parse(xhr.responseText);
+                    displayChannels(channels, 'userChannels');
+                } catch (e) {
+                    console.error("Błąd podczas przetwarzania odpowiedzi JSON: ", e);
+                }
+            } else {
+                console.error("Błąd przy pobieraniu kanałów użytkownika: ", xhr.status, xhr.responseText);
+            }
+        }
+    };
+    xhr.send();
+}
+
+function joinChannel() {
+    var channelId = document.getElementById('channelId').value;
+    var channelPassword = document.getElementById('channelPassword').value;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://webapp-oiihp.ondigitalocean.app/api/channels/join", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("x-access-token", accessToken);
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                console.log("Dołączono do kanału");
+                // Czyszczenie formularza
+                document.getElementById('channelId').value = '';
+                document.getElementById('channelPassword').value = '';
+                // Odświeżanie listy kanałów użytkownika
+                fetchUserChannels();
+            } else {
+                console.error("Błąd przy dołączaniu do kanału: ", xhr.status, xhr.responseText);
+            }
+        }
+    };
+
+    var data = JSON.stringify({
+        channelId: channelId,
+        password: channelPassword
+    });
+
+    xhr.send(data);
+}
+
+function selectChannel(channelId, isPublicChannel) {
+    // Usuń zaznaczenie ze wszystkich kanałów
+    var allSelectedChannels = document.querySelectorAll('.channel-button.selected');
+    allSelectedChannels.forEach(function(channel) {
+        channel.classList.remove('selected');
+    });
+
+    // Zaznacz aktualnie wybrany kanał
+    var selector = isPublicChannel ? '.public-channel' : '.user-channel';
+    var selectedChannel = document.querySelector(selector + '[data-channel-id="' + channelId + '"]');
+    if (selectedChannel) {
+        selectedChannel.classList.add('selected');
+    }
+
+    // Ukryj przycisk "Opuść kanał" i wyświetl przycisk "Kopiuj ID kanału"
+    if (isPublicChannel) {
+        document.getElementById('leaveChannelButton').style.display = 'none';
+        document.getElementById('copyChannelIdButton').style.display = 'block';
+        currentPublicChannelId = channelId; // Zapisz ID publicznego kanału
+    }
+
+    console.log("Wybrany kanał: " + channelId);
+}
+function leaveChannel() {
+    if (!currentChannelId) {
+        console.error("Nie wybrano żadnego kanału");
+        return;
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://webapp-oiihp.ondigitalocean.app/api/channels/leave?channelId=" + currentChannelId, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("x-access-token", accessToken);
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                console.log("Opuściliśmy kanał");
+                currentChannelId = null;
+                document.getElementById('leaveChannelButton').style.display = 'none';
+                fetchUserChannels();
+            } else {
+                console.error("Błąd przy opuszczaniu kanału: ", xhr.status, xhr.responseText);
+            }
+        }
+    };
+
+    xhr.send();
+}
+
+function copyChannelId() {
+    if (currentPublicChannelId) {
+        var channelIdInput = document.getElementById('channelId');
+        channelIdInput.value = currentPublicChannelId;
+    }
+}
+
+function fetchChannelMessages(channelId) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://webapp-oiihp.ondigitalocean.app/api/channels/channelMessages?channelId=" + channelId, true);
+    xhr.setRequestHeader("x-access-token", accessToken);
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                var response = JSON.parse(xhr.responseText);
+                console.log(response); // Logowanie odpowiedzi serwera
+                if (response.messages) {
+                    displayMessages(response.messages); // Załóżmy, że odpowiedź zawiera pole 'messages'
+                } else {
+                    console.error("Odpowiedź serwera nie zawiera pola 'messages'");
+                }
+            } else {
+                console.error("Błąd przy pobieraniu wiadomości: ", xhr.status, xhr.responseText);
+            }
+        }
+    };
+    xhr.send();
+}
+
+function displayMessages(messages) {
+    var messagesContainer = document.getElementById('messagesDisplay');
+    messagesContainer.innerHTML = '';
+
+    if (Array.isArray(messages)) {
+        messages.forEach(function(message) {
+            var messageDiv = document.createElement('div');
+            messageDiv.classList.add('message');
+            messageDiv.innerHTML = 
+                '<div class="message-sender">Nadawca: ' + message.sender + '</div>' +
+                '<div class="message-date">' + new Date(message.createdAt).toLocaleString() + '</div>' +
+                '<div class="message-content">' + message.content + '</div>';
+            messagesContainer.appendChild(messageDiv);
+        });
+    } else {
+        console.error("Otrzymane dane nie są tablicą: ", messages);
+    }
 }
--- welcome.php
+++ welcome.php
@@ -5,6 +5,10 @@ session_start();
 
 <html>
 <head>
+
+ <script>
+    var accessToken = "<?php echo $_SESSION['access_token']; ?>";
+ </script>
  <script src="js/welcome.js"></script>
 <link rel="stylesheet" type="text/css" href="css/welcome.css">
 <title>Witaj w komunikatorze</title>
@@ -19,13 +23,17 @@ session_start();
 		<div id="channels" class="channels-container">
         <input type="text" id="channelId" placeholder="ID kanału">
         <input type="password" id="channelPassword" placeholder="Hasło kanału (opcjonalne)">
-        <button onclick="joinChannel()">Dołącz do kanału</button>
+        <button onclick="joinChannel()" class="action-button">Dołącz do kanału</button>
+		
 		<div id="channels-left">
         <h3>Twoje kanały</h3>
         <div id="userChannels"></div>
         <h3>Kanały publiczne</h3>
         <div id="publicChannels"></div>
-		</div></div>
+		</div>
+		<button id="leaveChannelButton" class="action-button" onclick="leaveChannel()" style="display: none;">Opuść kanał</button>
+		<button id="copyChannelIdButton" class="action-button" style="display: none;" onclick="copyChannelId()">Kopiuj ID kanału</button>
+		</div>
 
 	
     <div id="messagesSection" class="messages-container">
